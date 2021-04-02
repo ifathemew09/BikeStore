86ed8dd214ec19ede9a32fc718a81c54d8571012
Ethan Benckwitz <60863350+ebenckwitz@users.noreply.github.com>
2021-04-02 09:29:36 -0600

    Ethan's attempts

---
+++ ExecuteTest.java
@@ -0,0 +1,44 @@
+package BikeStore1;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExecuteTest {
+
+	@Test
+	void readInventoryFromFileTest() {
+		Execute test = new Execute();
+		Bike mb = new Bike("mb", 32.3, 35.0, "red", 5, 3, 8);
+		Bike rb = new Bike("rb", 45.6, 50.0, "blue", 6, 2, 9);
+		Bike cb = new Bike("cb", 50.5, 60.0, "orange", 4, 1, 7);
+		Bike expected[] = [mb, rb, mb, mb, cb]; 
+		assertEquals(expected, Execute.readInventoryFromFile("Inventory.txt"));
+		assertNotNull(Execute.readInventoryFromFile("Inventory.txt"));
+	}
+	
+	@Test
+	void updateInventoryBasedOnClientsRequestsTest() {
+		Execute test = new Execute();
+		Bike mb = new Bike("mb", 32.3, 35.0, "red", 5, 3, 8);
+		Bike rb = new Bike("rb", 45.7, 51.2, "blue", 6, 2, 9);
+		Bike[] inventory = test.readInventoryFromFile("Inventory.txt");
+		
+		assertEquals(3600.0, test.updateInventoryBasedOnClientsRequests(inventory, "ClientRequest.txt", "BikeOrder.txt"));
+		assertTrue(test.updateInventoryBasedOnClientsRequests(inventory, "ClientRequest.txt", "BikeOrder.txt") == 3600.0);
+		assertNotSame(3600, test.updateInventoryBasedOnClientsRequests(inventory, "ClientRequest.txt", "BikeOrder.txt"));
+	
+	}
+	
+	@Test
+	void printInventoryTest() {
+		Execute test = new Execute();
+		Bike[] inventory = test.readInventoryFromFile("Inventory.txt");
+		
+		assertSame("Hello", test.printInventory(inventory));
+		assertNotSame(5, test.printInventory(inventory));
+	}
+
+}
